0d30c67310382112c3295f34f4408668834f3664
RampageousCarp <kirilbodnar2007@gmail.com>
2026-02-12 12:31:49 +0100

    json file relocation

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         // Dependency injection: wiring up our components
-        string filePath = "tasks.json";
+        string filePath = "Project1/Repository/JSON/tasks.json";
         ITaskRepository repository = new JsonTaskRepository(filePath);
         ITaskService service = new TaskService(repository);
         ITaskView view = new ConsoleTaskView(service);
