dca895cd2570631026fc350fa1246298358594d0
RampageousCarp <kirilbodnar2007@gmail.com>
2026-02-27 15:51:31 +0100

    Model Update

--- Project1/Models/TaskItem.cs
+++ Project1/Models/TaskItem.cs
@@ -1,7 +1,12 @@
+using Project1.Models.ENums;
+
 namespace Project1.Models;
 
 public class TaskItem {
-  public int Id { get; set; }
-  public required string Description { get; set; }
-  public bool Completed { get; set; }
+    public long Id { get; set; }
+    public required string Description { get; set; }
+    public Priority Priority { get; set; }
+    public Status Status { get; set; } = Status.NotStarted;
+    public bool Completed { get; set; } = false;
+    public DateTime CreatedAt { get; set; }
 } 
