f80a5f16566c28735f552fe8ff2b03b2bcaadb6a
RampageousCarp <kirilbodnar2007@gmail.com>
2026-03-03 12:19:24 +0100

    save task method updated

--- Project1/Repositories/JsonTaskRepository.cs
+++ Project1/Repositories/JsonTaskRepository.cs
@@ -1,43 +1,54 @@
 using System.Text.Json;
 using Project1.Models;
 using Project1.Repositories.Interfaces;
+using Project1.Services.Interfaces;
 
 namespace Project1.Repositories;
+
 class JsonTaskRepository : ITaskRepository
 {
     private readonly string _filePath;
     public JsonTaskRepository(string filePath) => _filePath = filePath;
+
     public TaskItem[] LoadTasks()
     {
         if (!File.Exists(_filePath))
         {
             return new TaskItem[0];
         }
+
         string json = File.ReadAllText(_filePath);
-        
+
         var tasksList = JsonSerializer.Deserialize<List<TaskItem>>(json);
-        
-        if (tasksList == null || tasksList.Count == 0) {
+
+        if (tasksList == null || tasksList.Count == 0)
+        {
             return new TaskItem[0];
         }
-        
+
         TaskItem[] tasks = new TaskItem[tasksList.Count];
         for (int i = 0; i < tasksList.Count; i++)
             tasks[i] = tasksList[i];
-        
+
         return tasks;
     }
-    
-    public void SaveTasks(TaskItem[] tasks, int count)
+
+    public void SaveTasks(IMyIterator<TaskItem> tasks, int count)
     {
-        List<TaskItem> tasksList = new List<TaskItem>();
-        for (int i = 0; i < count; i++)
-            tasksList.Add(tasks[i]);
-        
-        string json = JsonSerializer.Serialize(tasksList, new JsonSerializerOptions { 
-            WriteIndented = true 
+        TaskItem[] tasksArray = new TaskItem[count];
+
+        tasks.Reset();
+        int pos = -1;
+        while (tasks.HasNext())
+        {
+            tasksArray[++pos] = tasks.Next();
+        }
+
+        string json = JsonSerializer.Serialize(tasksArray, new JsonSerializerOptions
+        {
+            WriteIndented = true
         });
-        
+
         File.WriteAllText(_filePath, json);
     }
 }
