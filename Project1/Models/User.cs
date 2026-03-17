b9b6226f79784e4fc49ebdf6ffa7137575735edc
RampageousCarp <kirilbodnar2007@gmail.com>
2026-03-17 12:40:44 +0100

    user model added

---
+++ Project1/Models/User.cs
@@ -0,0 +1,9 @@
+namespace Project1.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+}
