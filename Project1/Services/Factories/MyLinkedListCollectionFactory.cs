c986dfea059a22171600475f7e7815da672cfb3a
RampageousCarp <kirilbodnar2007@gmail.com>
2026-04-09 15:54:06 +0200

    Collection type can be chosen in menu

--- Project1/Services/Factories/MyLinkedListCollectionFactory.cs
+++ Project1/Services/Factories/MyLinkedListCollectionFactory.cs
@@ -3,7 +3,9 @@ using Project1.Services.Interfaces;
 
 namespace Project1.Services.Factories;
 
-public class MyLinkedListCollectionFactory
+public class MyLinkedListCollectionFactory : IMyCollectionFactory
 {
     public IMyCollection<T> Create<T>() => new MyLinkedListCollection<T>();
+    public IMyCollection<T> Create<T>(IMyIterator<T> iterator) => new MyLinkedListCollection<T>(iterator);
+    
 }
--- Project1/Services/Interfaces/IMyCollectionFactory.cs
+++ Project1/Services/Interfaces/IMyCollectionFactory.cs
@@ -3,4 +3,5 @@ namespace Project1.Services.Interfaces;
 public interface IMyCollectionFactory
 {
     IMyCollection<T> Create<T>();
+    IMyCollection<T> Create<T>(IMyIterator<T> iterator);
 }
