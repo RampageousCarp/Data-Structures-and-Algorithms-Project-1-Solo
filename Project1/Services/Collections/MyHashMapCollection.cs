d92626b1bc748f7ea4d298df60c8a9fd4f7b8d24
RampageousCarp <kirilbodnar2007@gmail.com>
2026-04-16 15:39:01 +0200

    hashmap iterator implemented

--- Project1/Services/Collections/MyHashMapCollection.cs
+++ Project1/Services/Collections/MyHashMapCollection.cs
@@ -5,13 +5,26 @@ namespace Project1.Services.Collections;
 public class MyHashMapCollection<T> : IMyCollection<T>
 {
     
+    private Node?[] _buckets;
+    private int _count;
+    private bool _dirty;
+    private int _dirtyCount;
+
+    private const int DEFAULT_CAPACITY = 16;
+    private const double LOAD_FACTOR = 0.75;
+    
     public MyHashMapCollection()
     {
+        _buckets = new Node[DEFAULT_CAPACITY];
     }
 
     public MyHashMapCollection(IMyIterator<T> iterator)
     {
-        
+        _buckets = new Node[DEFAULT_CAPACITY];
+        while (iterator.HasNext())
+            Add(iterator.Next());
+
+        _dirty = false;
     }
 
     #region Methodes
@@ -81,10 +94,94 @@ public class MyHashMapCollection<T> : IMyCollection<T>
     
     
     #region InnerClasses
+    
     private class Node(T data)
     {
         public T Data = data;
         public Node? Next = null;
+    }
+    
+    private class MyHashMapCollectionIterator : IMyIterator<T>
+    {
+        private readonly MyHashMapCollection<T> _collection;
+        private int _bucketIndex;
+        private Node? _current;
+        private bool _started;
+        
+        public MyHashMapCollectionIterator(MyHashMapCollection<T> collection)
+        {
+            _collection = collection;
+        }
+        
+        public bool HasNext()
+        {
+            if (!_started)
+                return FindNextNode() != null;
+
+            if (_current?.Next != null)
+                return true;
+            
+            int savedIndex = _bucketIndex + 1;
+            int temp = savedIndex;
+            while (temp < _collection._buckets.Length)
+            {
+                if (_collection._buckets[temp] != null)
+                    return true;
+                temp++;
+            }
+            
+            return false;
+        }
+        
+        public T Next()
+        {
+            if (!_started)
+            {
+                _started = true;
+                _bucketIndex = 0;
+                _current = FindNextNode();
+                _bucketIndex++;
+                return _current!.Data;
+            }
+
+            if (_current?.Next != null)
+            {
+                _current = _current.Next;
+                return _current!.Data;
+            }
+
+            while (_bucketIndex < _collection._buckets.Length)
+            {
+                if (_collection._buckets[_bucketIndex] != null)
+                {
+                    _current = _collection._buckets[_bucketIndex];
+                    _bucketIndex++;
+                    return _current!.Data;
+                }
+                _bucketIndex++;
+            }
+
+            return default!;
+        }
+        
+        public void Reset()
+        {
+            _bucketIndex = 0;
+            _current = null;
+            _started = false;
+        }
+
+        private Node? FindNextNode()
+        {
+            while (_bucketIndex < _collection._buckets.Length)
+            {
+                if (_collection._buckets[_bucketIndex] != null)
+                    return _collection._buckets[_bucketIndex];
+                _bucketIndex++;
+            }
+
+            return null;
+        }
     }
     #endregion
 }
